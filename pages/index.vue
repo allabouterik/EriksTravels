a8abd8ff44e33a729c7cea7d344191d89f554b65
Eoin O'Malley <eoinom@gmail.com>
2024-01-13 19:05:22 +0000

    Update home page background image for Show Reel

--- pages/index.vue
+++ pages/index.vue
@@ -107,7 +107,7 @@ const onVideoClose = () => {
   position: relative;
   height: 100vh;
   width: 100vw;
-  background-image: url("/showreel.jpg");
+  background-image: url("/showreel2.jpg");
   background-size: cover;
 
   .playImgContainer {
