4896aae5f6ffa0ea8d63bbceb345828de1e1bff0
Eoin O'Malley <eoinom@gmail.com>
2024-04-27 23:48:38 +0100

    PosterLinkCarousel - add image box-shadow

--- components/PosterLinkCarouselSwiper.vue
+++ components/PosterLinkCarouselSwiper.vue
@@ -188,6 +188,7 @@ const onPosterLinkClick = (index) => (clickedPosterIndex.value = index);
     width: 100%;
     height: 100%;
     object-fit: contain;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
     z-index: -1;
 
     &:active,
